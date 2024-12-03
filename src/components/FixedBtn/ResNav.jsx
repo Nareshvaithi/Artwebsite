14f7eeb6ff4499f190f94d6579d5469e03fb740f
Nareshvaithi <nareshvaithi4@gmail.com>
2024-12-03 11:23:18 +0530

    admin icon

--- src/components/FixedBtn/ResNav.jsx
+++ src/components/FixedBtn/ResNav.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { ContextProvide } from "../../ContextAPI/CreateContext";
 import { NavLink } from "react-router-dom";
-
+import { TfiUser } from "react-icons/tfi";
 const ResNavbar = () => {
   const { menu } = useContext(ContextProvide);
 
@@ -16,8 +16,9 @@ const ResNavbar = () => {
     <div
       className={`w-full z-[99999] pt-32 h-screen px-3 lg:px-10 ${bgColor} fixed top-0 right-0 left-0 ${
         menu ? "opacity-100 visible" : "opacity-0 invisible"
-      } transition-all duration-1000 flex justify-center items-center`}
+      } transition-all duration-1000 flex flex-col lg:flex-row justify-center items-center`}
     >
+      <div className="w-full">
       <div className={`${bgColor} transition-all duration-1000`}>
         <div className="flex justify-between gap-5 items-center pb-10">
           <NavLink
@@ -25,14 +26,14 @@ const ResNavbar = () => {
             onMouseLeave={handleLeave}
             className="text-3xl lg:text-9xl text-transparent hover:text-white transition-colors duration-500 font-Aboreto outline-text"
           >
-            <span className="text-sm">1 </span>Home
+            <span className="text-sm">1 </span>HOME
           </NavLink>
           <NavLink
             onMouseEnter={() => handleHover("bg-blue-500")}
             onMouseLeave={handleLeave}
             className="text-3xl lg:text-9xl text-transparent hover:text-white transition-colors duration-500 font-Aboreto outline-text"
           >
-            <span className="text-sm">2 </span>About
+            <span className="text-sm">2 </span>ABOUT
           </NavLink>
         </div>
         <div className="flex justify-between gap-3 items-center pb-10">
@@ -41,14 +42,14 @@ const ResNavbar = () => {
             onMouseLeave={handleLeave}
             className="text-3xl lg:text-9xl text-transparent hover:text-white transition-colors duration-500 font-Aboreto outline-text"
           >
-            <span className="text-sm">3 </span>Artist
+            <span className="text-sm">3 </span>ARTIST
           </NavLink>
           <NavLink
             onMouseEnter={() => handleHover("bg-yellow-500")}
             onMouseLeave={handleLeave}
             className="text-3xl lg:text-9xl text-transparent hover:text-white transition-colors duration-500 font-Aboreto outline-text"
           >
-            <span className="text-sm">4 </span>Artwork
+            <span className="text-sm">4 </span>ARTWORK
           </NavLink>
         </div>
         <div className="flex justify-between gap-3 items-center pb-10">
@@ -57,16 +58,21 @@ const ResNavbar = () => {
             onMouseLeave={handleLeave}
             className="text-3xl lg:text-9xl text-transparent hover:text-white transition-colors duration-500 font-Aboreto outline-text"
           >
-            <span className="text-sm">5 </span>Gallery
+            <span className="text-sm">5 </span>GALLERY
           </NavLink>
           <NavLink
             onMouseEnter={() => handleHover("bg-pink-500")}
             onMouseLeave={handleLeave}
-            className="text-3xl lg:text-9xl text-transparent hover:text-white transition-colors duration-500 font-Aboreto outline-text"
+            className="text-3xl lg:text-9xl text-transparent hover:text-white transition-colors duration-500 font-serif outline-text"
           >
-            <span className="text-sm">6 </span>Contact
+            <span className="text-sm">6 </span>CONTACT
           </NavLink>
         </div>
+      </div>
+      </div>
+      <div className="bg-gray-400 h-screen w-[1px] hidden lg:block"></div>
+      <div className="text-white w-[80px] flex">
+        <div className="mx-auto"><TfiUser size={30} /></div>
       </div>
     </div>
   );
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -39,20 +39,14 @@ const Hero = () => {
                     </h1>
                 </div>
                 <div className="bg-transparent grid grid-cols-2 lg:grid-cols-2">
-                    <div data-rellax-mobile-speed="3" data-rellax-speed="5" className="rellax group bg-transparent border border-gray-400 hover:border-blue-400 rounded-full px-2 py-12 lg:py-16 h-fit w-fit mx-auto">
-                        <NavLink onMouseEnter={cursorEnter} onMouseLeave={cursorLeave} className={"bg-gray-100 group-hover:text-blue-400 px-6 py-12 lg:py-14 rounded-full text-xl lg:text-3xl font-Aboreto"}>Home</NavLink>
-                    </div>
                     <div data-rellax-mobile-speed="4" data-rellax-speed="4" className="rellax group bg-transparent border border-gray-400 hover:border-red-400 rounded-full px-2 py-12 lg:py-16 h-fit w-fit mx-auto">
                         <NavLink onMouseEnter={cursorEnter} onMouseLeave={cursorLeave} className={"bg-gray-100 group-hover:text-red-400 px-6 py-12 lg:py-14 rounded-full text-xl lg:text-3xl font-Aboreto"}>About</NavLink>
-                    </div>
-                    <div data-rellax-mobile-speed="3" data-rellax-speed="1" className="rellax group bg-transparent border border-gray-400 hover:border-yellow-400 rounded-full px-1 py-14 lg:py-20 h-fit w-fit col-span-2 mx-auto">
-                        <NavLink onMouseEnter={cursorEnter} onMouseLeave={cursorLeave} className={"bg-gray-100 group-hover:text-yellow-400 px-6 py-12 lg:py-20 rounded-full text-xl lg:text-4xl font-Aboreto"}>Gallery</NavLink>
                     </div>
                     <div data-rellax-mobile-speed="2" data-rellax-speed="4" className="rellax group bg-transparent border border-gray-400 hover:border-green-400 rounded-full px-1 py-12 lg:py-16 h-fit w-fit mx-auto">
                         <NavLink onMouseEnter={cursorEnter} onMouseLeave={cursorLeave} className={"bg-gray-100 group-hover:text-green-400 px-6 py-12 lg:py-14 rounded-full text-xl lg:text-3xl font-Aboreto"}>Artist</NavLink>
                     </div>
-                    <div data-rellax-mobile-speed="4" data-rellax-speed="2" className="rellax group bg-transparent border border-gray-400 hover:border-orange-400 rounded-full px-1 py-12 lg:py-16 h-fit w-fit mx-auto">
-                        <NavLink onMouseEnter={cursorEnter} onMouseLeave={cursorLeave} className={"bg-gray-100 group-hover:text-orange-400 px-4 py-12 lg:py-14 rounded-full text-lg lg:text-2xl font-Aboreto"}>Artwork</NavLink>
+                    <div data-rellax-mobile-speed="4" data-rellax-speed="2" className="rellax group bg-transparent border border-gray-400 hover:border-orange-400 rounded-full px-1 py-12 lg:py-20 h-fit w-fit col-span-2 mx-auto">
+                        <NavLink onMouseEnter={cursorEnter} onMouseLeave={cursorLeave} className={"bg-gray-100 group-hover:text-orange-400 px-4 py-14 lg:py-20 rounded-full text-lg lg:text-3xl font-Aboreto"}>Artwork</NavLink>
                     </div>
                     <div data-rellax-mobile-speed="2" data-rellax-speed="1" className="rellax group bg-transparent border border-gray-400 hover:border-purple-400 rounded-full px-1 py-12 lg:py-16 h-fit w-fit col-span-2 mx-auto">
                         <NavLink onMouseEnter={cursorEnter} onMouseLeave={cursorLeave} className={"bg-gray-100 group-hover:text-purple-400 px-4 py-12 lg:py-16 rounded-full text-lg lg:text-2xl font-Aboreto"}>Contact</NavLink>
